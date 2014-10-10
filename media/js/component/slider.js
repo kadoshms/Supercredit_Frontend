bb375e8afae29a2b346da9fc719375eec33eae4f
mor <kadoshms@gmail.com>
2014-10-10 16:14:40 +0300

    Finished form without events

---
+++ media/js/component/slider.js
@@ -0,0 +1,38 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'libs/mustache',
+  'libs/bootstrap-slider',
+  'text!templates/component/slider.mustache',
+], function($, _, Backbone,Mustache,BootstrapSlider,SliderTemplate){
+	var exports = {}
+	exports.SliderModel = Backbone.Model.extend({
+		setValue : function(value){
+			this.set("value",value)
+		},
+		getValue : function(){
+			return this.get("value")
+		}
+	});
+	exports.SliderView = Backbone.View.extend({
+		el : '#slider-container',
+		initialize: function(viewParams){
+			this.sliderOptions = viewParams.options;
+			this.model = viewParams.model
+			this.render()
+		},
+		render: function(){
+			var _options = this.sliderOptions;
+			var content = Mustache.to_html(SliderTemplate,{max:_options.max})
+			this.$el.html(content)
+			var _sliderContainer = this.$el.find('#slider')
+			var model = this.model;
+			_sliderContainer.slider(_options).
+				on('slideStop',function(e){
+					model.setValue(e.value)
+				});
+		}
+	});
+	return exports;
+});
--- media/js/views/main.js
+++ media/js/views/main.js
@@ -4,18 +4,24 @@ define([
   'backbone',
   'libs/mustache',
   'component/purchase-types',
+  'component/slider',
   'text!templates/main.mustache'
-], function($, _, Backbone,Mustache,PurchaseTypes,MainTemplate){
+], function($, _, Backbone,Mustache,PurchaseTypes,Slider,MainTemplate){
 	var exports = {}
 	exports.MainView = Backbone.View.extend({
 		el : '#main-content',
 		initialize: function(){
 		},
 		render: function(){
-			var _content = Mustache.to_html(MainTemplate,{test:'hello world'})
+			var _content = Mustache.to_html(MainTemplate,{})
 			this.$el.html(_content)
 			var _typesContainer = this.$el.find('#types')
 			var purchaseTypesView = new PurchaseTypes.PurchaseTypesView({el:_typesContainer})
+			var sliderModel = new Slider.SliderModel()
+			var slider = new Slider.SliderView({model:sliderModel,options:{
+				max 	: 2000,
+				handle	: 'square',
+			}})
 		}
 	});
 	return exports;
