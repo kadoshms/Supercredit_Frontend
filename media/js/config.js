df9f38a77323127a517d29ae0cfff344d826e5c2
mor <kadoshms@gmail.com>
2014-10-11 21:49:06 +0300

    Config change

--- media/js/config.js
+++ media/js/config.js
@@ -3,7 +3,7 @@ define([
   'underscore',
 ], function($, _){
 	var exports = {}
-	exports.url = "http://localhost:3000/api/";
+	exports.url = "http://supercredit.beecard.co/api/";
 	exports.STATUS_PURCHASE_DENIED 		= 1010;
 	exports.STATUS_PURCHASE_APPROVED 	= 1020;
 	exports.STATUS_NO_CREDENTIALS		= 1101;
